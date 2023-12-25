c2b2bf3dc01aa5765e6ee250efd47aa122d6317d
VladislavLipkinGit <lipkin1993vladislav@gmail.com>
2023-12-25 22:19:44 +0300

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import list.MyArrayList;
+import list.MyList;
+
+import java.util.Collection;
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyList<String> list = new MyArrayList<>();
+        list.add("2");
+        list.add("30");
+        list.add("20");
+        list.add("ggr");
+        System.out.println(list.get(0));
+        list.delete(1);
+        System.out.println(list);
+
+
     }
 }
